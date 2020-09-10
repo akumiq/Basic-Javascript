4229235efacb0cf8570ec50ac5b32bcc3a16a507
Miqdad <kun.miqdad23@gmail.com>
2020-09-10 08:23:21 +0700

    learning type data javascript

---
+++ type-data/index.js
@@ -0,0 +1,32 @@
+let name = "Fulan" //contoh pembuatan variable dengan type data string
+let countSantri = 50 //contoh pembuatan variable dengan type data number
+let isActive = true //contoh pembuatan variable dengan type data boolean
+
+//contoh pembuatan variable dengan type data array
+const dataArray = {
+    name: 'Fulan',
+    since: 2017
+}
+
+//contoh pembuatan variable dengan type data object
+const dataObject = [
+    {
+        name: 'Fulan',
+        since: 2017
+    },
+    {
+        name: 'Fulan',
+        since: 2018
+    },
+    {
+        name: 'Fulan',
+        since: 2019
+    }
+]
+
+//lalu cara untuk melihat type data variable tersebut bisa menggunakan
+typeof name
+typeof countSantri
+typeof isActive
+typeof dataArray
+typeof dataObject
